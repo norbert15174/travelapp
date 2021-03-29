637cffaffc72f1eb259073d45e228f4fcb463896
Faron Norbert <faronnorbertkrk@gmail.com>
2021-03-29 15:35:30 +0200

    Added FriendsService

---
+++ src/main/java/pl/travel/travelapp/services/FriendsService.java
@@ -0,0 +1,24 @@
+package pl.travel.travelapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.travel.travelapp.repositories.FriendMessagesRepository;
+import pl.travel.travelapp.repositories.FriendsRepository;
+import pl.travel.travelapp.repositories.LinkFriendsRepository;
+import pl.travel.travelapp.repositories.PersonalDataRepository;
+
+@Service
+public class FriendsService {
+
+    private PersonalDataRepository personalDataRepository;
+    private LinkFriendsRepository linkFriendsRepository;
+    private FriendsRepository friendsRepository;
+    private FriendMessagesRepository friendMessagesRepository;
+    @Autowired
+    public FriendsService(PersonalDataRepository personalDataRepository, LinkFriendsRepository linkFriendsRepository, FriendsRepository friendsRepository, FriendMessagesRepository friendMessagesRepository) {
+        this.personalDataRepository = personalDataRepository;
+        this.linkFriendsRepository = linkFriendsRepository;
+        this.friendsRepository = friendsRepository;
+        this.friendMessagesRepository = friendMessagesRepository;
+    }
+}
